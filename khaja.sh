6727aa7778ae26b5bbe969a6da9936f466885e49
Cloud User <ec2-user@ip-172-31-32-95.ap-south-1.compute.internal>
2022-08-20 04:49:03 +0000

    khaja.sh

---
+++ khaja.sh
@@ -0,0 +1,4 @@
+jhvkjbk kubvewkbv.,v  vew.,evkniugvebiubvvl.bbj.
+vjwbjhb vkj vkjklvenl n lvn jnl mkjwn lklknveknwdn
+vjbjkbv lk v n iewiewno;evoe elnei;viknvnkln lk  
+wvljbkjwbbbbbbbbbbbbbbbbbbbbbb evken nnnnnnlekhiieieihihe
